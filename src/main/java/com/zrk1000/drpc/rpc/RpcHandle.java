e9f0f01dc8fdcd5752a5d57cfb9531ca327e9021
zrk1000 <zrk1000@163.com>
2017-04-20 00:55:38 +0800

    完成80%

---
+++ src/main/java/com/zrk1000/drpc/rpc/RpcHandle.java
@@ -0,0 +1,15 @@
+package com.zrk1000.drpc.rpc;
+
+
+import com.zrk1000.drpc.proxy.ServiceMethod;
+
+import java.io.Closeable;
+
+/**
+ * Created by zrk-PC on 2017/4/1.
+ */
+public interface RpcHandle extends Closeable {
+
+    Object exec(ServiceMethod serviceMethod, Object[] args);
+
+}
