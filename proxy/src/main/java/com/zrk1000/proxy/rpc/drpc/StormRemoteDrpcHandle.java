52c6dc86c57699965f6f5129af4f209971785ae9
zrk1000@163.com <kang12345>
2017-04-26 20:32:12 +0800

    处理冲突

--- proxy/src/main/java/com/zrk1000/proxy/rpc/drpc/StormRemoteDrpcHandle.java
+++ proxy/src/main/java/com/zrk1000/proxy/rpc/drpc/StormRemoteDrpcHandle.java
@@ -1,78 +1,12 @@
-<<<<<<< HEAD:proxy/src/main/java/com/zrk1000/proxy/rpc/drpc/StormRemoteDrpcHandle.java
-package com.zrk1000.proxy.rpc.drpc;
+package com.zrk1000.drpc.rpc.drpc;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zrk1000.proxy.proxy.ServiceMethod;
 import com.zrk1000.proxy.rpc.RpcHandle;
-import org.apache.commons.pool2.impl.GenericObjectPool;
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import org.apache.storm.Config;
-import org.apache.storm.utils.DRPCClient;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * Created by rongkang on 2017-04-02.
- */
-public class StormRemoteDrpcHandle implements RpcHandle {
-
-
-    private GenericObjectPool<DRPCClient> drpcClientPool ;
-
-    private Map<String,Set<String>> drpcServiceMap ;
-
-    public StormRemoteDrpcHandle(Config conf, String stormHost, Integer stormPort,Integer stormTimeout,GenericObjectPoolConfig poolConfig,Map<String,Set<String>> drpcServiceMap) {
-        DrpcClientFactory factory = new DrpcClientFactory(conf, stormHost, stormPort, stormTimeout);
-        this.drpcClientPool = new GenericObjectPool<DRPCClient>(factory,poolConfig);
-        this.drpcServiceMap = drpcServiceMap;
-    }
-
-    public Object exec(ServiceMethod serviceMethod, Object[] args) {
-        DRPCClient client = null;
-        String result = null;
-        DrpcResponse drpcResponse = new DrpcResponse();
-        try{
-            drpcClientPool.borrowObject();
-            String drpcService = getDrpcService(serviceMethod.getClazz());
-            if(drpcService == null)
-                throw  new RuntimeException("未匹配到的远程drpc，请检查配置");
-            result = client.execute(drpcService, new DrpcRequest(serviceMethod.getClazz(),serviceMethod.getMethodName(),serviceMethod.hashCode(),args).toJSONString());
-            if(result!=null)
-                drpcResponse = JSON.parseObject(result, DrpcResponse.class);
-        }catch (Exception e){
-            e.printStackTrace();
-            drpcResponse.setCode(500);
-            drpcResponse.setMsg(e.getMessage());
-        }finally {
-            drpcClientPool.returnObject(client);
-        }
-
-        return JSONObject.parseObject(JSON.toJSONString(drpcResponse.getData()), serviceMethod.getReturnType());
-    }
-
-    public String getDrpcService(String clazz) {
-        for(String key:drpcServiceMap.keySet())
-            if(drpcServiceMap.get(key).contains(clazz))
-                return key;
-        return null;
-    }
-
-    public void close() throws IOException {
-        System.out.println("==============do close!");
-    }
-
-
-}
-=======
-package com.zrk1000.drpc.rpc.drpc;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.zrk1000.drpc.proxy.ServiceMethod;
-import com.zrk1000.drpc.rpc.RpcHandle;
+import com.zrk1000.proxy.rpc.drpc.DrpcClientFactory;
+import com.zrk1000.proxy.rpc.drpc.DrpcRequest;
+import com.zrk1000.proxy.rpc.drpc.DrpcResponse;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.apache.storm.Config;
@@ -152,4 +86,4 @@ public class StormRemoteDrpcHandle implements RpcHandle {
 
 
 }
->>>>>>> 5a89aacc8fbce7a8672038e7466e7619f9535f2d:src/main/java/com/zrk1000/drpc/rpc/drpc/StormRemoteDrpcHandle.java
+
