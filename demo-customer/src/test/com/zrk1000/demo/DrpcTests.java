e91ef345b5b015390bb4d9ca4b08957b1d4a2162
zrk1000@163.com <kang12345>
2017-04-28 20:07:30 +0800

    test

---
+++ demo-customer/src/test/com/zrk1000/demo/DrpcTests.java
@@ -0,0 +1,16 @@
+package com.zrk1000.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zrk-PC
+ * Date: 2017/4/28
+ * Time: 19:56
+ */
+public class DrpcTests {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+    
